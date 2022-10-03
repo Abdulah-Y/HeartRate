bf2a4ec3d49d53df69b55fe637070a36c26494b2
Abdulah-Y <t1yuab00@students.oamk.fi>
2022-10-03 23:35:18 +0300

    Tehtävä "Heart rate limits calculator"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+ const [age, setAge] = useState('')
+ const[Heartratelimits, setHeartratelimits] = useState(0)
+
+ const laske = (e) => {
+  e.preventDefault()
+  const tulos = ((220-age) * 0.85) 
+  setHeartratelimits(tulos)
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='tausta'>
+      <h3>Heart rate limits calculator</h3>
+      <form onSubmit={laske}>
+        <div>
+          <label>Age</label>
+          <input type="number" value={age} onChange={e => setAge(e.target.value)}/>
+        </div>
+        <div>
+          <label>Heart rate limits</label>
+          <output>{Heartratelimits.toFixed(0)}</output>
+        </div>
+        <button>Laske</button>
+      </form>
     </div>
+ 
   );
 }
 
 export default App;
+
